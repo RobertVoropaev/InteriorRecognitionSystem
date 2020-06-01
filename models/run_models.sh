f1603fd2463fa44da8476c51b520505851b48776
Robert Voropaev <voropaev@bigone.demist.ru>
2020-06-01 11:41:11 +0300

    learn unet 1 2 3

--- models/run_models.sh
+++ models/run_models.sh
@@ -3,7 +3,7 @@ python3 Unet_1_64_512.py -mt sigmoid
 python3 Unet_2_64_512.py -mt sigmoid
 
 python3 Unet_3_64_512.py -mt sigmoid
-python3 Unet_3_64_512.py -mt softmax
+python3 Unet_3_64_512.py -mt softmax -la softmax
 
 python3 Unet_4_64_512.py -mt sigmoid
 
