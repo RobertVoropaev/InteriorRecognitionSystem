a5aeb18b4fd5d85a2db8ad7452f75f3ffd7dc1e8
RobertVoropaev <robertvoropaev11@gmail.com>
2020-06-05 19:00:25 +0300

    new 2048 model

---
+++ models/run_models_2048_1.sh
@@ -0,0 +1,3 @@
+python3 Unet_2048.py -lib 1 -mt 1 -cvd 1
+python3 Unet_2048.py -lib 2 -mt 2 -cvd 1
+python3 Unet_2048.py -lib 3 -mt 3 -cvd 1
---
+++ models/run_models_2048_2.sh
@@ -0,0 +1,2 @@
+python3 Unet_2048.py -lib 4 -mt 4 -cvd 0
+python3 Unet_2048.py -lib 5 -mt 5 -cvd 0
