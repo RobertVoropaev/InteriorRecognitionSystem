08ca1b3677225130d9911f93828e47d2e0b78768
Robert Voropaev <voropaev@bigone.demist.ru>
2020-06-07 08:38:22 +0300

    upd

--- models/run_models_batch_norm.sh
+++ models/run_models_batch_norm.sh
@@ -1,2 +1,2 @@
-Unet_1024_batch_norm.py -lib 3 -mt 3
-Unet_1024_batch_norm_after.py -lib 3 -mt 3
+python3 Unet_1024_batch_norm.py -lib 3 -mt 3 -bs 10
+python3 Unet_1024_batch_norm_after.py -lib 3 -mt 3 -bs 10
--- models/run_models_dropout.sh
+++ models/run_models_dropout.sh
@@ -1,5 +1,5 @@
-Unet_1024_dropout.py -lib 3 -mt 3_dp_01 -dp 0.1
-Unet_1024_dropout.py -lib 3 -mt 3_dp_02 -dp 0.2
-Unet_1024_dropout.py -lib 3 -mt 3_dp_03 -dp 0.3
-Unet_1024_dropout.py -lib 3 -mt 3_dp_04 -dp 0.4
-Unet_1024_dropout.py -lib 3 -mt 3_dp_05 -dp 0.5
+python3 Unet_1024_dropout.py -lib 3 -mt 3_dp_01 -dp 0.1 -bs 10
+python3 Unet_1024_dropout.py -lib 3 -mt 3_dp_02 -dp 0.2 -bs 10
+python3 Unet_1024_dropout.py -lib 3 -mt 3_dp_03 -dp 0.3 -bs 10
+python3 Unet_1024_dropout.py -lib 3 -mt 3_dp_04 -dp 0.4 -bs 10
+python3 Unet_1024_dropout.py -lib 3 -mt 3_dp_05 -dp 0.5 -bs 10
