7465f8234db0d93a579ab708cc8f1103eaa76a23
Robert Voropaev <voropaev@bigone.demist.ru>
2020-06-09 02:22:36 +0300

    learn dropout

--- models/run_models_batch_trans.sh
+++ models/run_models_batch_trans.sh
@@ -1,3 +1,3 @@
 #python3 Unet_1024_batch_norm.py -lib 3 -mt 3 -bs 10
-python3 Unet_1024_conv_trans.py -lib 3 -mt 3 -bs 10
+#python3 Unet_1024_conv_trans.py -lib 3 -mt 3 -bs 10
 python3 Unet_1024.py -lib 3 -mt 3 -bs 10
